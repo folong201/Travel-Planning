a97977b37412c0b278d37d20d91b9b3159dfe435
Emmanuella-Menkahe <teumelise@gmail.com>
2024-09-06 17:40:37 +0200

    login signup and logout back

--- Back/BookingPlan/accounts/urls.py
+++ Back/BookingPlan/accounts/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('signup/', views.signup_view, name='signup'),
     path('', views.home_view, name='home'),
+    path('destination/', views.dashboard_view, name='destination'),
+    path('agency/', views.agency_view, name='agency'),
+    path('stays/', views.stays_view, name='stays'),
+    path('schedule/', views.stays_view, name='schedule'),
+    path('tips/', views.tips_view, name='tips'),
+    path('base/', views.base_view, name='base'),
 
 ]
--- Back/BookingPlan/accounts/views.py
+++ Back/BookingPlan/accounts/views.py
@@ -7,12 +7,11 @@ def signup_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         email = request.POST['email']
-        password1 = request.POST['password1']
-        password2 = request.POST['password2']
-        user = User.objects.create_user(username=username, email=email, password1=password1, password2=password2)
+        password = request.POST['password']
+        user = User.objects.create_user(username=username, email=email, password=password)
         user.save()
-        return redirect('destination.html')
-    return render(request, 'signup.html')
+        return redirect('destination')
+    return render(request, 'pages/signup.html')
 
 def login_view(request):
     if request.method == 'POST':
@@ -21,16 +20,35 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('dashboard')
+            return redirect('destination')
         else:
-            return render(request, 'login.html', {'error': 'Wrong username or password'})
-    return render(request, 'login.html')
+            return render(request, 'pages/login.html', {'error': 'Wrong username or password'})
+    return render(request, 'pages/login.html')
 
 def logout_view(request):
     logout(request)
-    return redirect('login.html')
+    return redirect('login')
 
 
 def home_view(request):
     return render(request, 'index.html')
+
+def dashboard_view(request):
+    return render(request, 'pages/UserDashboard/destination.html')
+
+def agency_view(request):
+    return render(request, 'pages/UserDashboard/agency.html')
+
+def stays_view(request):
+    return render(request, 'pages/UserDashboard/stays.html')
+
+def schedule_view(request):
+    return render(request, 'pages/UserDashboard/schedule.html')
+
+def tips_view(request):
+    return render(request, 'pages/UserDashboard/tips.html')
+
+def base_view(request):
+    return render(request, 'pages/UserDashboard/base.html')
+
 
